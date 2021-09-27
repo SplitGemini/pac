33228157aaaaf0b82207cc40dfe688f925d0a63e
SplitGemini <764402868@qq.com>
2021-09-27 21:13:31 +0800

    weekly file prepare

---
+++ pac2auto_proxy_rule.py
@@ -0,0 +1,24 @@
+import re
+import os
+import json
+
+#daiy_path = os.getcwd()
+daiy_path = '../gh-pages/pac/whitelist.pac'
+white_list = "whitelist.pac"
+
+# patterns
+china_domains_pattern = r"(?<=china_domains = )\{.+?\}"
+
+
+with open(os.path.join(daiy_path, white_list), "r", encoding="utf-8") as f:
+    result = re.search(china_domains_pattern, f.read(), re.S)
+
+if result != None:
+    jsonObj = json.loads(result.group())
+    lines =  []
+    for key in jsonObj:
+        lines.append(key)
+    with open(white_list, "w", encoding="utf-8") as f:
+        f.write('@@||' + '\n@@||'.join(lines))
+else:
+    print("not matched")
